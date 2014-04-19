5ccdf1393e9a8e8290c521eaff06c1fed5e84b4d
cwdyang <davidy@datacom.co.nz>
2014-04-20 00:21:56 +1200

    Dave doing some Phase1 stuff

--- GITTestProject/Program.cs
+++ GITTestProject/Program.cs
@@ -11,6 +11,7 @@ namespace GITTestProject
 		static void Main(string[] args)
 		{
 			//DAVIDY added this main trunk branch
+			//I am adding something for Phase 1 (same working directory switched from Master to Phase1)
 		}
 	}
 }
